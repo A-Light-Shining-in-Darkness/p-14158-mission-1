092e1c536229ac28a2aa59781600eb7aa2943e20
A-Light-Shining-in-Darkness <kor2002rea@naver.com>
2026-07-16 16:31:35 +0900

    Java에서 Kotlin으로 전환

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    java
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    kotlin("plugin.jpa") version "2.2.21"
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -8,9 +10,10 @@ group = "com"
 version = "0.0.1-SNAPSHOT"
 description = "p-14158-1"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
     }
 }
 
@@ -22,15 +25,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    // compileOnly("org.projectlombok:lombok")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
-    // annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    // testCompileOnly("org.projectlombok:lombok")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    // testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
---
+++ src/main/kotlin/com/back/domain/post/post/entity/Post.kt
@@ -0,0 +1,21 @@
+package com.back.domain.post.post.entity
+
+import com.back.domain.member.member.entity.Member
+import com.back.global.jpa.entity.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Post(
+        @ManyToOne
+        var author: Member,
+        var title: String,
+        @Column(columnDefinition = "TEXT")
+        var content: String
+) : BaseEntity() {
+
+    override fun toString(): String {
+        return "Post(id=$id, title='$title', content='$content')"
+    }
+}
